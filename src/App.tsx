d0619e697a6de16f88631462394a69267841678b
Mohammed Arif <arif.sg.my@gmail.com>
2025-02-09 13:28:01 +0800

    Icon Change

--- src/App.tsx
+++ src/App.tsx
@@ -462,7 +462,7 @@ function App() {
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 z-50"
       >
-        <img src="/wpicon.svg" alt="WhatsApp" className="h-6 w-6" />
+        <img src="wpicon.svg" alt="WhatsApp" className="h-6 w-6" />
       </a>
 
       {/* Cookie Consent */}
