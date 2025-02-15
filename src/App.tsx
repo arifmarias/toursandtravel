a67e65e793cf077e38c71b9f305fed16b21a64aa
Mohammed Arif <arif.sg.my@gmail.com>
2025-02-15 17:44:33 +0800

    Icon change-wp

--- src/App.tsx
+++ src/App.tsx
@@ -471,7 +471,11 @@ function App() {
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 z-50"
       >
-        <WhatsappIcon className="h-6 w-6" />
+        <img 
+          src="/assets/whatsapp.png" 
+          alt="WhatsApp"
+          className="h-6 w-6"
+        />
       </a>
 
       {/* Cookie Consent */}
