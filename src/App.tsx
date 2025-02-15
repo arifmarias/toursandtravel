af29e1aa411fd9cec79b08b4f6d5f5507c9540f6
Mohammed Arif <arif.sg.my@gmail.com>
2025-02-15 17:47:52 +0800

    iconChnage-wp

--- src/App.tsx
+++ src/App.tsx
@@ -472,7 +472,7 @@ function App() {
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 z-50"
       >
         <img 
-          src="/assets/whatsapp.png" 
+          src="https://static-00.iconduck.com/assets.00/whatsapp-icon-1020x1024-iykox85t.png" 
           alt="WhatsApp"
           className="h-6 w-6"
         />
